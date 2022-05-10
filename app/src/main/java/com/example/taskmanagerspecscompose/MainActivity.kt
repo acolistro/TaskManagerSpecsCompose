6dc8a572e149d66b845864df5722fc963070a33d
acolistro <arcolistro@gmail.com>
2022-05-10 07:36:26 -0700

    Update composables details, alignment still needs correction

--- app/src/main/java/com/example/taskmanagerspecscompose/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerspecscompose/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,6 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanagerspecscompose.ui.theme.TaskManagerSpecsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,8 +46,19 @@ fun TaskStatusText(taskComplete: String, congrats: String) {
                 .wrapContentWidth(Alignment.CenterHorizontally)
                 .wrapContentHeight(Alignment.CenterVertically)
         )
-        Text(text = taskComplete)
-        Text(text = congrats)
+        Text(
+            text = taskComplete,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+            )
+        Text(
+            text = congrats,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .wrapContentWidth(Alignment.CenterHorizontally)
+            )
     }
 }
 
